df5dcde3e82b658ec5cf1e5e72d50d0ce693598b
Voo Tzuen Hau <vince_voo@yahoo.com>
2024-04-15 23:28:31 +0800

    completed the model section

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -8,6 +8,7 @@ import * as THREE from "three";
 import { Canvas } from "@react-three/fiber";
 import { View } from "@react-three/drei";
 import { models, sizes } from "../constants";
+import { animateWithGsapTimeline } from "../utils/animation";
 
 const Model = () => {
   const [size, setSize] = useState("small");
@@ -32,9 +33,17 @@ const Model = () => {
 
   useEffect(() => {
     if (size === "large") {
+      animateWithGsapTimeline(tl, small, smallRotation, "#view1", "#view2", {
+        transform: "translateX(-100%)",
+        duration: 2,
+      });
     }
 
     if (size === "small") {
+      animateWithGsapTimeline(tl, large, largeRotation, "#view2", "#view1", {
+        transform: "translateX(0)",
+        duration: 2,
+      });
     }
   }, [size]);
 
